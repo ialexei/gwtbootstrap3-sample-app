958e4298ccc8d20d537d4aa0ef679148b3a4d957
Alexei Isac <alexei@aidev.org>
2015-12-09 15:38:10 -0500

    cleaned up sample

--- src/main/java/org/gwtbootstrap3/sample/client/SampleWebApp.java
+++ src/main/java/org/gwtbootstrap3/sample/client/SampleWebApp.java
@@ -1,13 +1,19 @@
 package org.gwtbootstrap3.sample.client;
 
+import org.gwtbootstrap3.extras.fullcalendar.client.ui.CalendarConfig;
+import org.gwtbootstrap3.extras.fullcalendar.client.ui.Event;
+import org.gwtbootstrap3.extras.fullcalendar.client.ui.FullCalendar;
+import org.gwtbootstrap3.extras.fullcalendar.client.ui.ViewOption;
+
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.core.client.JavaScriptObject;
 import com.google.gwt.uibinder.client.UiBinder;
-import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.Widget;
 
 /**
@@ -19,21 +25,30 @@ public class SampleWebApp extends Composite implements EntryPoint {
 
     private static MyUiBinder myUiBinder = GWT.create(MyUiBinder.class);
     
+    @UiField
+    SimplePanel tableContainer;
+    
     public void onModuleLoad() {
         RootPanel.get().add(myUiBinder.createAndBindUi(this));
+        
+        FullCalendar calendar = new FullCalendar(
+				"test", 
+				ViewOption.month, 
+				new CalendarConfig(),
+				true);
+		
+		calendar.setWidth("400px");
+		calendar.addEvent(new Event(getEventProvider()));
+		tableContainer.add(calendar);
     }
     
-    @UiHandler("showPopupButton")
-    public void onShowPopupButtonClicked(ClickEvent clickEvent) {
-    	
-    	final MyModal myModal = new MyModal(new DialogContent(), "Lazy loading events sample");
-    	myModal.getSaveButton().addClickHandler(new ClickHandler() {
-			
-			public void onClick(ClickEvent event) {
-				myModal.hide();
-			}
-		});
-    	
-    	myModal.show();
+    public static void instanceFoo(String test) {
+    	Window.alert(test);
     }
+    
+    public native final JavaScriptObject getEventProvider() /*-{
+		return function(start, end, timezone, callback) {
+			@org.gwtbootstrap3.sample.client.SampleWebApp::instanceFoo(Ljava/lang/String;)('hello');
+		};
+	}-*/;
 }
